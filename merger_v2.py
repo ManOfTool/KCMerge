34ecfcc80b5b706b87006e821670221f635074d7
ToolOfMan <a101013322@gmail.com>
2019-09-02 00:47:25 +0800

    No more resizing, choose the biggest one

--- merger_v2.py
+++ merger_v2.py
@@ -14,62 +14,30 @@ saved_name: saved name with subname
 '''
 
 def Merging(mode, src_path, saved):
-    width, height = 0, 0
-    x_offset, y_offset = 0, 0
-    x, y = 0, 0
 
     images = appendFile(src_path)
     n_imgs = len(images)
+    x, y = modeSelect(mode, n_imgs)
 
-    # Resizeing
-    for i in images:
-        width += i.width
-        height += i.height
-
-    width //= n_imgs
-    height //= n_imgs
-    print('[.]Images resize to {} x {}'.format(width, height))
-
-    for i in range(len(images)):
-        images[i] = images[i].resize((width, height))
-
-    # Selecting mode
-    if mode == 'hl':
-        print('[.]Mode: Horizontal, Linear')
-        x = n_imgs
-        y = 1
-
-    elif mode == 'hw':
-        print('[.]Mode: Horizontal, Wrap')
-        x = ceil(n_imgs / 2)
-        y = ceil(n_imgs / x)
-
-    elif mode == 'lv':
-        print('[.]Mode: Vertical, Linear')
-        x = 1
-        y = n_imgs
-
-    elif mode == 'vw':
-        print('[.]Mode: Vertical, Wrap')
-        y = ceil(n_imgs / 2)
-        x = ceil(n_imgs / y)
-
-    else:
-        print('[!]Unknow mode')
-        exit()
-
-    # Final windth, height
-    width = images[0].width * x
-    height = images[0].height * y
+    width, height = 0, 0
+    x_offset, y_offset = 0, 0
 
     print('[.]x: {}, y: {}'.format(x, y))
 
     # Create image template
-    template_img = Image.new('RGB', (width, height), color=0xffffff)
+    for i in images:
+        if i.width > width:
+            width = i.width
+        if i.height > height:
+            height = i.height
+
+    template_img = Image.new('RGB', (width * x, height * y), color=0xffffff)
+
+    # Merging images
     cnt = 0
     for i in range(x):
         for j in range(y):
-            template_img.paste(images[cnt], (x_offset + images[0].width * i, y_offset + images[0].height * j))
+            template_img.paste(images[cnt], (x_offset + width * i, y_offset + height * j))
             print('[.]Image{} pasted'.format(cnt))
 
             cnt += 1
@@ -95,6 +63,35 @@ def appendFile(src_path):
             print('[-]{} might not be a image. Check carefully!'.format(img))
 
     return images
+
+def modeSelect(mode, n_imgs):
+    x, y = 0, 0
+
+    if mode == 'hl':
+        print('[.]Mode: Horizontal, Linear')
+        x = n_imgs
+        y = 1
+
+    elif mode == 'hw':
+        print('[.]Mode: Horizontal, Wrap')
+        x = ceil(n_imgs / 2)
+        y = ceil(n_imgs / x)
+
+    elif mode == 'lv':
+        print('[.]Mode: Vertical, Linear')
+        x = 1
+        y = n_imgs
+
+    elif mode == 'vw':
+        print('[.]Mode: Vertical, Wrap')
+        y = ceil(n_imgs / 2)
+        x = ceil(n_imgs / y)
+
+    else:
+        print('[!]Unknow mode')
+        exit()
+
+    return x, y
 
 if __name__ == "__main__":
     args = sys.argv
